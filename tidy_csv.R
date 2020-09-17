6370b6ba1287f4cbd1abcb37e5fc964878e0b78a
davidjayjackson <davidjayjackson@gmail.como>
2020-09-17 13:01:23 -0400

    Updated Sept. 17, 2020

--- tidy_csv.R
+++ tidy_csv.R
@@ -8,18 +8,48 @@ library(lubridate)
 ##
 rm(list=ls())
 db <- dbConnect(SQLite(), dbname="../COVIDDB/COVID.sqlite3")
+##
+## Read US Daily COVID-19 Data for each day
+##
 CSSE <-dir("../DATA/csse/",full.names=T) %>% map_df(data.table::fread)
 CSSE <-CSSE %>% clean_names()
-CSSE$last_update <- gsub("/","-",CSSE$last_update)
-CSSE$last_update <- substr(CSSE$last_update,1,10)
-CSSE$last_update <- as.Date(CSSE$last_update,format="%Y-%m-%d" )
+USA <- CSSE %>% filter(country_region=="US")
+USA$last_update <- gsub("/","-",USA$last_update)
+USA$Ymd <- substr(USA$last_update,1,10)
+USA$last_update <- as.Date(USA$last_update,format="%Y-%m-%d" )
+USA <- USA[order(USA$last_update,decreasing = FALSE)]
 
-CSSE$last_update <- as.character(CSSE$last_update)
-
-db <- dbConnect(SQLite(),dbname="../COVIDDB/COVID.sqlite3")
-dbWriteTable(db,"CSSE",CSSE,row.names=FALSE,overwrite=TRUE)
+### 
+### Calc Ohio
+###
+Ohio <- CSSE %>% filter(province_state == "Ohio")
+ggplot(Ohio) + geom_line(aes(x=last_update,y=deaths))
 ##
-## Calc and Plot  Daily Cases and Deaths
+## Calc Daily Grand Totals
 ##
 USA <- aggregate(confirmed~last_update,data=CSSE,FUN=sum)
-ggplot(USA) + geom_line(aes(x=last_update,y=confimed))
+ggplot(USA) + geom_line(aes(x=last_update,y=confirmed)) +
+  labs(title="US Accum Cases")
+
+USAD <- aggregate(deaths~last_update,data=CSSE,FUN=sum)
+ggplot(USAD) + geom_line(aes(x=last_update,y=deaths)) +
+  labs(title="US Accum Deaths")
+
+## Calc and Plot  Daily Cases and Deaths
+##
+DCases <- diff(USA$confirmed,lag=1)
+df <- USA[2:147,]
+DD <- as.data.table(DCases)
+df$DD <-DD
+df$Cases <- DD$DCasesdf$confirmed - DD$DCases
+##
+ggplot(df) +geom_line(aes(x=last_update,y=Cases,col="Deaths")) +
+  geom_smooth(aes(x=last_update,y=Cases,col="Lm"),method="lm")
+  
+##
+## Create SQLite table
+##
+CSSE$last_update <- as.character(CSSE$last_update)
+db <- dbConnect(SQLite(),dbname="../COVIDDB/COVID.sqlite3")
+dbWriteTable(db,"CSSE",CSSE,row.names=FALSE,overwrite=TRUE)
+  ##
